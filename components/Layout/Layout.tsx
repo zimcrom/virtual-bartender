08a9968fe09353e1e3b039a6ebe6643df776c020
Morgan Henning <megannh6349@gmail.com>
2024-02-05 20:32:07 -0500

    added favicon

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,14 @@
 import Header from "../Header";
 import Footer from "../Footer";
 import React from 'react'
+import Head from "next/head";
 
 function Layout({ children }: { children: React.ReactNode}) {
   return (
     <>
+    <Head>
+    <link rel="shortcut icon" type="image/png" href="favicon48.png" />
+    </Head>
       <Header />
       <main>
         {children}
